76e81a97f284002b43727f2c5d3571f7e218c8bb
Taras Greben <taras.greben@gmail.com>
2021-04-26 00:10:55 +0300

    avoid fail on init if ports.json wrong

--- src/init.lua
+++ src/init.lua
@@ -3,7 +3,7 @@ cf = {}
 tmr.create():alarm(2000, 0, function()
 	if not cjson then _G.cjson = sjson end
 	local _, b = node.bootreason()
-	if b == 0 then require("fpUpdate")() end
+	if b == 0 then pcall(function() require("fpUpdate")() end) end
 	require("connect")(function()
 		tmr.create():alarm(100, 0, function()
 			require("http")
